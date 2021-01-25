5f3de6089035653027a27ef3d08f5a7fc30a515e
Dmytro Hurskyi <dima4365@gmail.com>
2021-01-25 19:54:15 +0200

    added style.css, changed index.html and base.html

--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 
-# Create your views here.
-
 def main(request):
-    return render(request, 'index.html')
+    page_title = "Restaurant — Index"
+    # В {} передаём переменные (контекст) в разметку
+    return render(request, 'index.html', {'page_title': page_title})
+
