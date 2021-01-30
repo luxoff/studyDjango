78e3e97538ee0eb683889b1eafc67ee63ebda939
Dmytro Hurskyi <dima4365@gmail.com>
2021-01-30 18:42:32 +0200

    added a few models, edited view and index page

--- main/models.py
+++ main/models.py
@@ -1,3 +1,35 @@
+import os
+from uuid import uuid4
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=40)
+    category_order = models.IntegerField()
+    is_visible = models.BooleanField(default=True)
+    is_special = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.title} : {self.category_order}'
+
+
+class MenuItem(models.Model):
+
+    def get_file_name_menuItem(self, filename):
+        ext = filename.split('.')[-1]
+        filename = f'{uuid4()}.{ext}'
+        return os.path.join('images/menu_items/', filename)
+
+    title = models.CharField(max_length=50)
+    photo = models.ImageField(upload_to=get_file_name_menuItem)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
+    ingredients = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    is_visible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.title}'
+
--- main/views.py
+++ main/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 
+from .models import Category, MenuItem
+
 
 def main(request):
+    categories = Category.objects.filter(is_visible=True).order_by("category_order")
     page_title = "Restaurant — Index"
+    for item in categories:
+        item.items = MenuItem.objects.filter(category=item.pk)
     # В {} передаём переменные (контекст) в разметку
-    return render(request, 'index.html', {'page_title': page_title})
-
+    return render(request, 'index.html', {'page_title': page_title, 'categories': categories})
