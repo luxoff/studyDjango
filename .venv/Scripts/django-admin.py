a66da43963369a6ae8f5b1549786fb6b3bfdaae4
Dmytro Hurskyi <dima4365@gmail.com>
2021-02-23 14:04:34 +0200

    setting up heroku

--- .venv/Scripts/django-admin.py
+++ .venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!D:\Projects\django\studyDjango\.venv\Scripts\python.exe
+#!D:\Projects\django\src\studyDjango\.venv\Scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
