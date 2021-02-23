f8a9884ad9289cb82dd514bd462d9a28052d272a
luxoff <39244489+luxoff@users.noreply.github.com>
2021-02-23 14:58:02 +0200

    Update settings.py

--- studyDjango/settings.py
+++ studyDjango/settings.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'studyDjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR + '/templates']
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
